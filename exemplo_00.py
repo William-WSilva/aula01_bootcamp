5b1e1259cf5453b6ab3ddecdeec34a4adb939675
William Wsilva <williamreis.wsilva@gmail.com>
2024-07-14 16:08:11 -0300

    introdução ao python

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,8 @@
-print("Hello World")
+# Crie um programa que o usuario digite o nome, salario e bonus, com as informações imprima a saudação com o  nome do usuario e valor da salario com o bonus
+nome_usuario = input("Digiete seu nome: ")
+salario = float(input("Digiete seu salario: "))
+bonus_salario = float(input("Digiete seu bonus: "))
+CONSTANTE_BONUS = 1000
+valor_bonus = CONSTANTE_BONUS + salario * bonus_salario
+
+print(F"O usuario {nome_usuario} possui o bonus de {valor_bonus}")
